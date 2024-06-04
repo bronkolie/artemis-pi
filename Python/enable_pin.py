2e36610ec0884c033639297fb40aecb687dd0795
Branko Lucassen <branko.lucassen@gmail.com>
2024-06-04 15:27:14 +0200

    The relay now starts as disabled instead of enabled

--- Python/enable_pin.py
+++ Python/enable_pin.py
@@ -8,10 +8,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.OUT)
 try:
     while True:
-        GPIO.output(PIN, GPIO.HIGH)
-        input("Press enter to disable...")
         GPIO.output(PIN, GPIO.LOW)
         input("Press enter to enable...")
+        GPIO.output(PIN, GPIO.HIGH)
+        input("Press enter to disable...")
+
 except KeyboardInterrupt:
     print("\nInterrupted. Exiting...")
 finally:
